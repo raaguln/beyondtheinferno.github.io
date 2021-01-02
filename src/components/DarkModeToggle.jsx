4dfe261a1f3877482e0cd9efce1ecf9b8c2ef980
Raagul N <raagul72@gmail.com>
2021-01-03 00:38:15 +0530

    Dark Mode toggle animation

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTransition, animated as a } from 'react-spring'
 import Sun from '../assets/sun.svg'
 import Moon from '../assets/moon.svg'
 import styles from './DarkModeToggle.module.css'
 
-const DarkModeToggle = ({ mode = 'DARK_MODE' }) => {
-  const color = mode === 'LIGHT_MODE' ? '#F7D78352' : '#1C1C1E'
+const DarkModeToggle = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const transitions = useTransition(darkMode, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
   return (
     <button
       className={styles.button}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: darkMode ? '#262626' : '#F7D78352' }}
+      onClick={() => setDarkMode((prev) => !prev)}
     >
-      <img
-        className={styles.image}
-        src={ mode === 'LIGHT_MODE' ? Sun : Moon}
-        alt='Dark mode toggle'
-      />
+    {
+      transitions.map(({ item, props }, i) =>
+      item
+        ? <a.img
+            key={i}
+            className={styles.image}
+            src={Moon}
+            alt='Dark mode toggle'
+            style={props}
+          />
+        : <a.img
+            key={i}
+            className={styles.image}
+            src={Sun}
+            alt='Dark mode toggle'
+            style={props}
+          />
+      )
+    }
     </button>
   )
 }
